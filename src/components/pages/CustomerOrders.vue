e3cd036eb6d05135ac342a15fe84fc2f50c0b632
wcc723 <agito723@gmail.com>
2018-06-27 11:55:02 +0800

    加入購物車

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -20,12 +20,54 @@
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button type="button" class="btn btn-outline-secondary btn-sm"
+              @click="getProduct(item.id)">
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               查看更多
             </button>
-            <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button type="button" class="btn btn-outline-danger btn-sm ml-auto"
+              @click="addtoCart(item.id)">
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
+      aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ product.title }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.image" class="img-fluid" alt="">
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">{{ product.description }}</footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">{{ product.origin_price }} 元</div>
+              <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
+              <div class="h4" v-if="product.price">現在只要 {{ product.price }} 元</div>
+            </div>
+            <select name="" class="form-control mt-3" v-model="product.num">
+              <option :value="num" v-for="num in 10" :key="num">
+                選購 {{num}} {{product.unit}}
+              </option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計 <strong>{{ product.num * product.price }}</strong> 元
+            </div>
+            <button type="button" class="btn btn-primary"
+              @click="addtoCart(product.id, product.num)">
+              <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i>
               加到購物車
             </button>
           </div>
@@ -36,10 +78,16 @@
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
   data() {
     return {
       products: [],
+      product: {},
+      status: {
+        loadingItem: '',
+      },
       isLoading: false,
     };
   },
@@ -54,9 +102,46 @@ export default {
         vm.isLoading = false;
       });
     },
+    getProduct(id) {
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      vm.status.loadingItem = id;
+      this.$http.get(url).then((response) => {
+        vm.product = response.data.product;
+        $('#productModal').modal('show');
+        console.log(response);
+        vm.status.loadingItem = '';
+      });
+    },
+    addtoCart(id, qty = 1) {
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      vm.status.loadingItem = id;
+      const cart = {
+        product_id: id,
+        qty,
+      };
+      this.$http.post(url, { data: cart }).then((response) => {
+        console.log(response);
+        vm.status.loadingItem = '';
+        vm.getCart();
+        $('#productModal').modal('hide');
+      });
+    },
+    getCart() {
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+      vm.isLoading = true;
+      this.$http.get(url).then((response) => {
+        // vm.products = response.data.products;
+        console.log(response);
+        vm.isLoading = false;
+      });
+    },
   },
   created() {
     this.getProducts();
+    this.getCart();
   },
 };
 </script>
