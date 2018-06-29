d36d325283e1bf043fbcd98308d69f10d7751f7f
wcc723 <agito723@gmail.com>
2018-06-29 18:12:47 +0800

    套用 Bootstrap Dashboard 版型

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navbar/>
+    <div class="container-fluid">
+      <div class="row">
+        <Sidebar></Sidebar>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <router-view></router-view>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+export default {
+  components: {
+    Sidebar,
+    Navbar,
+  },
+};
+</script>
